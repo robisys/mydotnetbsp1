d449cd2600655ea835c324f15e0fad288f721918
Robert P <robisys@users.noreply.github.com>
2017-06-08 09:44:00 +0200

    Create build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env bash
-# dotnet restore && dotnet build
-# dotnet new --info
-dotnet  --info
-dotnet  new --help
-dotnet  test --help
-# chmod +x ./src/bsp1/build.sh
-#cd src/bsp1
-#dotnet restore
-#dotnet run
 echo "---- aspnetcoreapp ----"
-#cd ../bsp2
 cd bsp1    
 dotnet restore
 dotnet run
